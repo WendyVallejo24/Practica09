4f253c4fd6c0328650b4f0b5c45e67f4c4517193
Wendy Vallejo <wbvalpat24@gmail.com>
2023-05-05 10:57:15 -0500

    rename to programa09

--- src/main/java/org/uv/programa09/Programa09Application.java
+++ src/main/java/org/uv/programa09/Programa09Application.java
@@ -1,13 +1,13 @@
-package org.uv.Practica09;
+package org.uv.programa09;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Practica09Application {
+public class Programa09Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Practica09Application.class, args);
+		SpringApplication.run(Programa09Application.class, args);
 	}
 
 }
