8662f1e9391fcb093a2b77d23ea7ea57ff4969b8
Wendy Vallejo <wbvalpat24@gmail.com>
2023-05-08 10:47:10 -0500

    update programa09

---
+++ src/main/java/org/uv/programa09/controller/RepositoryEmpleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.uv.programa09.controller;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author wbpat
+ */
+public interface RepositoryEmpleado extends CrudRepository<Empleado, Long> {
+    
+}
