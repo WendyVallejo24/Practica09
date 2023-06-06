b02627a9170cecbad05f29d8d6f904391bbc2d6a
WendyVallejo24 <wbvalpat24@gmail.com>
2023-06-06 15:50:32 -0600

    update controller programa09

--- src/main/java/org/uv/programa09/controller/Controller.java
+++ src/main/java/org/uv/programa09/controller/Controller.java
@@ -4,7 +4,6 @@
  */
 package org.uv.programa09.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.uv.programa09.controller.Empleado;
 
 /**
  *
